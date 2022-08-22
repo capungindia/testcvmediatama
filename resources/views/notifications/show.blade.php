1e652b7fadb32013d89b472e8708e0ddc99aaffe
capungindia <108322286+capungindia@users.noreply.github.com>
2022-08-22 10:49:17 +0700

    Back button url
    change back button href property

--- resources/views/notifications/show.blade.php
+++ resources/views/notifications/show.blade.php
@@ -9,7 +9,7 @@
                 </div>
                 <div class="col-sm-6">
                     <a class="btn btn-default float-right"
-                       href="{{ url()->previous() }}">
+                       href="{{ route('home') }}">
                         Back
                     </a>
                 </div>
